8e80cf61dc4953263f6acb55ba67548964561557
Bruce Sena <bruce.sena@gmail.com>
2026-08-10 21:45:32 -0300

    rascunho de projeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # desafio_grupo_16
 Aplicativo Móvel - Programação para Dispositivos Móveis
+
+App Ionic
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Suporte e Vendas',
+  webDir: 'www'
+};
+
+export default config;
